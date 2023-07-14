e54cb0d2f4f60f803752339ce84a0e96b0d4cee2
Tom Renner <tom.renner@navvis.com>
2023-07-14 11:08:26 +0200

    Require CLI arguments

--- main.go
+++ main.go
@@ -7,5 +7,10 @@ import (
 
 func main() {
 	args := os.Args
+
+	if len(args) < 2 {
+		fmt.Println("This function requires command-line-args")
+		os.Exit(1)
+	}
 	fmt.Printf("Hello world\nas.Args: %v\nArguments: %v\n", args, args[1:])
 }
