7ebfea575b7e3ede519181fcfe85fb4480d5ae98
Tom Renner <tom.renner@navvis.com>
2023-07-14 12:15:39 +0200

    Make HTTP request to a server

--- main.go
+++ main.go
@@ -2,6 +2,10 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"net/url"
 	"os"
 )
 
@@ -9,8 +13,26 @@ func main() {
 	args := os.Args
 
 	if len(args) < 2 {
-		fmt.Println("This function requires command-line-args")
+		fmt.Println("Usage: ./http-get <url>")
 		os.Exit(1)
 	}
-	fmt.Printf("Hello world\nas.Args: %v\nArguments: %v\n", args, args[1:])
+
+	if _, err := url.ParseRequestURI(args[1]); err != nil {
+		fmt.Printf("URL is invalid: %s\n", err)
+		os.Exit(1)
+	}
+
+	response, err := http.Get(args[1])
+	if err != nil {
+		log.Fatal(err)
+	}
+	// defer keyword ensures this is run at the end of this method
+	defer response.Body.Close()
+
+	body, err := io.ReadAll(response.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("Status: %d\nBody: %s\n", response.StatusCode, body)
 }
