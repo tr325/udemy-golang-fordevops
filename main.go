502510b940711ceb824bebbab95381d4e5e370d7
Tom Renner <tom.renner@navvis.com>
2023-07-14 15:06:33 +0200

    Partial unmarshalling JSON; switch, for, maps

--- main.go
+++ main.go
@@ -13,10 +13,17 @@ import (
 
 // Using capitals for the type and fields ensures they are exported, and are available to other packages.
 // lower case is not exported.
+type Page struct {
+	Name string `json:"page"`
+}
+
 type Words struct {
-	Page  string   `json:"page"`
 	Input string   `json:"input"`
 	Words []string `json:"words"`
+}
+
+type Occurrences struct {
+	Words map[string]int `json:"words"`
 }
 
 func main() {
@@ -48,11 +55,33 @@ func main() {
 		os.Exit(1)
 	}
 
-	var words Words
-	err = json.Unmarshal(body, &words)
+	// Note: we are parsing the JSON _partially_ here, since we know that both endpoints include the
+	// `page` key, and then parsing the rest of the payload separately in the switch statement below
+	var page Page
+	err = json.Unmarshal(body, &page)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	fmt.Printf("JSON Parsed:\nPage: %s\nWords: %s\n", words.Page, strings.Join(words.Words, ", "))
+	switch page.Name {
+	case "words":
+		var words Words
+		err = json.Unmarshal(body, &words)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Printf("JSON Parsed:\nPage: %s\nWords: %s\n", page.Name, strings.Join(words.Words, ", "))
+	case "occurrence":
+		var occurrences Occurrences
+		err = json.Unmarshal(body, &occurrences)
+		if err != nil {
+			log.Fatal(err)
+		}
+		for word, count := range occurrences.Words {
+			fmt.Printf("%s: %d\n", word, count)
+		}
+	default:
+		fmt.Println("Page not found")
+	}
+
 }
